ab2ceaf12ea3c4dbbf01b894304729416b8cd3b0
kho903 <gmldnr2222@naver.com>
2020-12-20 18:37:33 +0900

    이항연산(2) - 산술연산자

---
+++ chap03/src/sec04/exam01_arithmetic/InputDataCheckNaNExample.java
@@ -0,0 +1,16 @@
+package sec04.exam01_arithmetic;
+
+public class InputDataCheckNaNExample {
+
+	public static void main(String[] args) {
+		String userInput = "NaN";
+		 
+		double val = Double.valueOf(userInput);
+		System.out.println(val);
+
+		double currentBalance = 10000.0;
+		currentBalance = currentBalance + val;
+		System.out.println(currentBalance);
+	}
+
+}
