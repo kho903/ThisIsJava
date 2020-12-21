997876c041d0b74dc31b764530f3e71fd8d69461
kho903 <gmldnr2222@naver.com>
2020-12-21 20:23:48 +0900

    반복문(for문, while문, 사용자입력)

---
+++ chap04/src/sec03/exam02_while/WhileKeyControlExample.java
@@ -0,0 +1,31 @@
+package sec03.exam02_while;
+
+public class WhileKeyControlExample {
+
+	public static void main(String[] args) throws Exception {
+		boolean run = true;
+		int speed = 0;
+		int keyCode = 0;
+		while (run) {
+			if (keyCode != 13 && keyCode != 10) {
+				System.out.println("-------------------------");
+				System.out.println("1. 증속 | 2. 감속 | 3. 중지");
+				System.out.println("-------------------------");
+				System.out.print("선택 : ");
+			}
+			// 사용자 입력 - 키보드의 keyCode
+			keyCode = System.in.read();
+
+			if (keyCode == 49) {
+				speed++;
+				System.out.println("현재 속도 = " + speed);
+			} else if (keyCode == 50) {
+				speed--;
+				System.out.println("현재 속도 = " + speed);
+			} else if (keyCode == 51) {
+				run = false;
+			}
+		}
+		System.out.println("프로그램 종료");
+	}
+}
