6909435aa1ab04a3de0e94213ad966167c196812
kho903 <gmldnr2222@naver.com>
2020-12-26 15:57:48 +0900

    메소드 호출

---
+++ Chap06/src/sec08_exam03_method_call/CarExample.java
@@ -0,0 +1,15 @@
+package sec08_exam03_method_call;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car myCar = new Car();
+
+		myCar.keyTurnOn();
+		myCar.run();
+
+		int speed = myCar.getSpeed();
+		System.out.printf("ÇöÀç  : %dkm/h\n", speed);
+	}
+
+}
