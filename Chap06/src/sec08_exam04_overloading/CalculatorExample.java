6bef5268a3c90c3e87309d31893a7bbe78ab7570
kho903 <gmldnr2222@naver.com>
2020-12-26 16:10:26 +0900

    메소드 오버로딩(Overloading)

---
+++ Chap06/src/sec08_exam04_overloading/CalculatorExample.java
@@ -0,0 +1,14 @@
+package sec08_exam04_overloading;
+
+public class CalculatorExample {
+
+	public static void main(String[] args) {
+		Calculator myCalc = new Calculator();
+
+		double result1 = myCalc.areaRectangle(10);
+		double result2 = myCalc.areaRectangle(10, 20);
+		System.out.printf("정사각형의 넓이 : %.1f\n", result1);
+		System.out.printf("직사각형의 넓이 : %.1f\n", result2);
+	}
+
+}
