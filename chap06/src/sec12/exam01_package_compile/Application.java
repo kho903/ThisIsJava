378e9873af51db00c71e4ffe3224852a361cbfa5
kho903 <gmldnr2222@naver.com>
2020-12-27 14:58:27 +0900

    패키지(cmd에서 컴파일)

---
+++ chap06/src/sec12/exam01_package_compile/Application.java
@@ -0,0 +1,11 @@
+package sec12.exam01_package_compile;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("애플리케이션을 실행합니다.");
+	}
+//  cmd에서 컴파일	
+//	javac -d . Application.java
+//	java sec12.exam01_package_compile.Application
+}
