f5c55a785138b1e65f1d8617ef298a90e897fd92
kho903 <gmldnr2222@naver.com>
2021-01-01 17:38:25 +0900

    익명 구현 객체, 다중 인터페이스 구현 클래스

---
+++ chap08/src/sec03/exam02_noname_implement_class/RemoteControlExample.java
@@ -0,0 +1,27 @@
+package sec03.exam02_noname_implement_class;
+
+import sec03.exam01_name_implement_class.RemoteControl;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		RemoteControl rc = new RemoteControl() {
+
+			@Override
+			public void turnOn() {
+				
+			}
+
+			@Override
+			public void turnOff() {
+				
+			}
+
+			@Override
+			public void setVolume(int volume) {
+				
+			}
+			
+		};
+		
+	}
+}
---
+++ chap08/src/sec03/exam03_multi_implement_class/RemoteControlExample.java
@@ -0,0 +1,15 @@
+package sec03.exam03_multi_implement_class;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		SmartTelevision tv = new SmartTelevision();
+		
+		RemoteControl rc = tv;
+		rc.turnOn();
+		rc.turnOff();
+		rc.setVolume(10);
+		
+		Searchable searchable = tv;
+		searchable.search("아아아아아아");
+	}
+}
