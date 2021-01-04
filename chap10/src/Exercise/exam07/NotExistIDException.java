905a1d6a92ed840d84df5ef221457a00c2d72c2f
kho903 <gmldnr2222@naver.com>
2021-01-04 18:34:50 +0900

    10장 예외처리 확인문제

---
+++ chap10/src/Exercise/exam07/NotExistIDException.java
@@ -0,0 +1,10 @@
+package Exercise.exam07;
+
+public class NotExistIDException extends Exception {
+	public NotExistIDException() {
+	}
+
+	public NotExistIDException(String message) {
+		super(message);
+	}
+}
