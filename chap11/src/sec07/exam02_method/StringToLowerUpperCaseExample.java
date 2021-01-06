e125059abdacff502807ad07509e2264ee84ab12
kho903 <gmldnr2222@naver.com>
2021-01-06 20:45:21 +0900

    String method - (indexOf(), length(), replace(), toLowerCase(), toUpperCase(), equalsIgnoreCase(), trim(), valueOf())

---
+++ chap11/src/sec07/exam02_method/StringToLowerUpperCaseExample.java
@@ -0,0 +1,17 @@
+package sec07.exam02_method;
+
+public class StringToLowerUpperCaseExample {
+	public static void main(String[] args) {
+		String str1 = "Java Programming";
+		String str2 = "JAVA PROGRAMMING";
+
+		System.out.println(str1.equals(str2)); // false
+
+		String lowerStr1 = str1.toLowerCase();
+		String lowerStr2 = str2.toLowerCase();
+		System.out.println(lowerStr1.equals(lowerStr2)); // true
+
+		// 대소문자 상관없이 비교하는 메소드
+		System.out.println(str1.equalsIgnoreCase(str2)); // true
+	}
+}
