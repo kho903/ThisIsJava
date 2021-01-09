568da1c6133b9f0fdd3195e76dee627557732e75
kho903 <gmldnr2222@naver.com>
2021-01-09 14:07:22 +0900

    Date, Calendar 클래스

---
+++ chap11/src/sec14/exam01_date/DateExample.java
@@ -0,0 +1,16 @@
+package sec14.exam01_date;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateExample {
+	public static void main(String[] args) {
+		Date now = new Date();
+		String strNow1 = now.toString();
+		System.out.println(strNow1);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 mm월 dd일 hh시 mm분 ss초");
+		String strnow2 = sdf.format(now);
+		System.out.println(strnow2);
+	}
+}
