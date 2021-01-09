fe9893af454a78d526c2a8a639ecc28f44b264e9
kho903 <gmldnr2222@naver.com>
2021-01-09 22:24:33 +0900

    java.time 패키지 - 파싱과 포맷팅

---
+++ chap11/src/sec16/exam01_java_time/DateTimeFormatExample.java
@@ -0,0 +1,13 @@
+package sec16.exam01_java_time;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DateTimeFormatExample {
+	public static void main(String[] args) {
+		LocalDateTime now = LocalDateTime.now();
+		DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy년 mm월 d일 h시 m분");
+		String nowString = now.format(dateTimeFormatter);
+		System.out.println(nowString);
+	}
+}
